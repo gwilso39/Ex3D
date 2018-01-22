10e61c4b1078427d4d5acbebbb266f18884e9081
Greg Wilson <greg.wilson5@outlook.com>
2018-01-22 16:54:57 -0500

    show added pseudo code for where and what methods to create and also a new switch case segment

--- Ex3D_DoubleEntryAccounting/Program.cs
+++ Ex3D_DoubleEntryAccounting/Program.cs
@@ -10,45 +10,62 @@ namespace Ex3D_DoubleEntryAccounting
     {
         static void Main(string[] args)
         {
-            //Execute method asking user with a menu "where would you like to go?"
+            //Execute method asking user with a menu "where would you like to go?" and return thier selection
             int selectedItem = selectMainMenuItem();
 
             //Possibly break this out as a method of it's own and test for valid input...
-            /*if ((selectedItem <= 0) || (selectedItem >=5))
+            //will this run back to the top from the default?  May need to convert this to a loop???
+            switch (selectedItem)
             {
-                Console.WriteLine("Please select an item from the menu as 1 - 4"); 
-            }*/
+                case 1:
+                    //call Check Balances Method
+                    break;
+                case 2:
+                    //call View Accounts Method
+                    break;
+                case 3:
+                    //call Enter Transactions Method
+                    break;
+                case 4:
+                    //call View Reports Method
+                    break;
+                default:
+                    Console.WriteLine("Please select an item from the menu as 1 - 4");
+                    break;
+            }
             Console.WriteLine($"You chose item number: {selectedItem}");
-            
 
-            //menu items include "Check Balances" "View Account" "Enter a Transaction" "View Reports"
 
-            //Under Check Balances show the following:
+            //Method to Check Balances show the following:
             //each of the 7 accounts and their current balances
             //upon completion of checking balance, enter 1 to return to main menu
 
-            //under View Account show the following:
+            //Method to View Account Listing for further selection
             //which account would you like?  show a screen with 7 accounts with numbers to select
             //enter 1 to return to main menu
-            //once an account is selected, display that account ledger
+
+            //Method or Loop or Case to display the appropriate account ledger
             //after ledger is displayed, enter 1 to return to account listing
 
-            //if enter a transaction is selected, ask through the transation for the items:
+            //Method to enter a transaction, ask through the transation for the items:
             //ID(key performatted, Date, Description (free text), USD, Acct to Credit, Acct to Debit
             //as a stretch goal - be able to split a transation between accounts
             //after last item is entered for entry, ask to save transaction or discard
             //if saved, push transaction to database
             //ask user to "enter 1 for main menu" and "enter 2 for another transaction"
 
-            //if View reports is selected, show a menu of available reports
+            //Method to show a menu of reports available and return which one is selected
             //continue to use item 1 as return to main menu
             //What are some standard reports needed?  
             //Balance Sheet (Assets=Liabilities + Capitol)
             //Income Statement (Revenue Expenses Gains Losses)
             //Equity Statment (Equity changes)            
+
+            //Method to compile individual reports (each?)
         }
 
-        //this will be a method to call the main menu item to the screen
+        //Method to display the main menu items to the screen and return user's selection
+        //menu items include "Check Balances" "View Account" "Enter a Transaction" "View Reports"
 
         static int selectMainMenuItem ()
         {
